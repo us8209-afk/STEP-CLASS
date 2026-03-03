719cb5dcd2c5c6fefefbc917f881f409fb1ae2ec
Uzair professional <us8209@srmist.edu.in>
2026-03-04 02:17:17 +0530

    UC2: ASCII banner implementation

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp UC2 - OOPS Banner Display Application
+ * @author Uzair Sikwani
+ * @version 2.0
+ */
+
+public class OOPSBannerApp {
+        public static void main(String[] args){
+            System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "  + "   ***** ");
+            System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "  + " **      ");
+            System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "  + "**       ");
+            System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "  + "  *****  ");
+            System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "  + "      ** ");
+            System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "  + " **   ** ");
+            System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "  + "  *****  ");
+        }
+    }
