16c4b734473fc8a53aefd781622cd07856962d50
Uzair professional <us8209@srmist.edu.in>
2026-03-04 01:49:15 +0530

    Printing OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * @author Uzair Sikwani
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args){
+        System.out.println("OOPS");
+        }
+    }
