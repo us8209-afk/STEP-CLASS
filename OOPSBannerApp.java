915e7dc4b9ca773f90bd2d0996af3c7e84cfcca9
Uzair professional <us8209@srmist.edu.in>
2026-03-04 02:32:08 +0530

    UC4: Array-based banner printing

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,21 @@
 /**
- * OOPSBannerApp UC3 - OOPS Banner Display Application
+ * OOPSBannerApp UC4 - OOPS Banner Display Application
  * @author Uzair Sikwani
- * @version 3.0
+ * @version 4.0
  */
 
 public class OOPSBannerApp {
     public static void main(String[] args){
-        System.out.println(String.join(" ","   ***   ","   ***   ","******   ","   ***** "));
-        System.out.println(String.join(" "," **   ** "," **   ** ","**    ** "," **      "));
-        System.out.println(String.join(" ","**     **","**     **","**     **","**       "));
-        System.out.println(String.join(" ","**     **","**     **","**    ** ","  *****  "));
-        System.out.println(String.join(" ","**     **","**     **","******   ","      ** "));
-        System.out.println(String.join(" "," **   ** "," **   ** ","**       "," **   ** "));
-        System.out.println(String.join(" ","   ***   ","   ***   ","**       ","  *****  "));
+        String[] lines=new String[7];
+        lines[0]=(String.join(" ","   ***   ","   ***   ","******   ","   ***** "));
+        lines[1]=(String.join(" "," **   ** "," **   ** ","**    ** "," **      "));
+        lines[2]=(String.join(" ","**     **","**     **","**     **","**       "));
+        lines[3]=(String.join(" ","**     **","**     **","**    ** ","  *****  "));
+        lines[4]=(String.join(" ","**     **","**     **","******   ","      ** "));
+        lines[5]=(String.join(" "," **   ** "," **   ** ","**       "," **   ** "));
+        lines[6]=(String.join(" ","   ***   ","   ***   ","**       ","  *****  "));
+        for(String line : lines){
+            System.out.println(line);
+        }
     }
 }
